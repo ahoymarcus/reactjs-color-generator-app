2a8379cc41fdd73608f407ed6b018ed082eeb29d
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-24 10:33:18 -0300

    Criando estrutura Jsx basica de App.js e definindo 03 variaveis de estado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 // https://www.youtube.com/watch?v=ly3m6mv5qvg
 // 3 hr 09' 00 ''
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from 'react';
 
-import Values from 'values.js'
+import SingleColor from './SingleColor';
+import Values from 'values.js';
+
+
 
 function App() {
-  return <h2>color generator project</h2>
+  const [ color, setColor ] = useState();
+  const [ error, setError ] = useState(false);
+  const [ list, setList ] = useState([]);
+
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        
+        <section className="colors">
+          <h4>list goes here</h4>
+        </section>
+      </section>
+    </>
+  );
 }
 
+
 export default App
+
