24aec5767cd629b8fd6295a23ee89cf0bc3f07f1
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-24 12:24:28 -0300

    Criar condicao para definir classe CSS de cor da fonte. Refatorar o uso da propriedade Hex renderizada para concatenar a hashtag.

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import rgbToHex from './utils'
 
 
-const SingleColor = ({ rgb, weight, hexColor, index }) => {
+const SingleColor = ({ rgb, weight, type, hexColor, index }) => {
   const [ alert, setAlert ] = useState(false);
 
 
@@ -17,13 +17,19 @@ const SingleColor = ({ rgb, weight, hexColor, index }) => {
   // 2. Usar uma função utilitária para converter o valor RGB para Hex
   const hex = rgbToHex(...rgb);
 
+  
+  //console.log(weight);
+  // Outra forma de mudar a classe CSS
+  // ${index > 10 && 'color-light'}
+
+  const hexValue = `#${hexColor}`;
 
   return (
-    <article className={`color`} style={{backgroundColor:`rgb(${bcg})` }}>
+    <article className={`color ${type === 'shade' ? 'color-light' : 'color-value' }`} style={{backgroundColor:`rgb(${bcg})` }}>
       <p className="percent-value">
         {weight}
       </p>
-      <p className="color-value">{hexColor}</p>
+      <p className="color-value">{hexValue}</p>
     </article>
   );
 }
