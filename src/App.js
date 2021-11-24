458daf58aa1363e5a86a1b8d1de9edac750b86b8
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-23 22:47:01 -0300

    Iniciar repositorio local

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// https://www.youtube.com/watch?v=ly3m6mv5qvg
+// 3 hr 09' 00 ''
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+
+import Values from 'values.js'
+
+function App() {
+  return <h2>color generator project</h2>
+}
+
+export default App
